c098cff8eff6b1902cdae667d6a0c94f4167aa0d
mygit <purceladzedavit@gmail.com>
2023-02-15 18:32:20 +0400

    route guards

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="confirmInput">confirm</button>
+  <button @click="saveChanges">save changes</button>
   <ul>
     <user-item
       v-for="user in users"
@@ -18,10 +19,33 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {
+      changesSaved: false,
+    };
+  },
   methods: {
     confirmInput() {
       this.$router.push('/teams'); // if we want to go on different page after doing some logic after click on button
     },
+    saveChanges() {
+      this.changesSaved = true;
+    },
+  },
+  //we can use navigation guard inside of the component also
+  beforeRouteEnter(to, from, next) {
+    next();
+  },
+
+  //with beforeRouteLeave we can make some actions before user
+  //leaves page
+  beforeRouteLeave(to, from, next) {
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm('Are you sure? You got unsaved changes');
+      next(userWantsToLeave);
+    }
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -26,11 +26,38 @@ const router = createRouter({
         },
       ], //so teammembers component is child of teamlist component and we have to register it inside teamlis (parent component), also we can to register children routes inside of children routes
     },
-    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
+    {
+      path: '/users',
+      components: { default: UsersList, footer: UsersFooter },
+      // we can run any functions we want before enter users component
+      beforeEnter(to, from, next) {
+        next();
+      },
+    },
     // { path: '/teams/:teamID', component: TeamMembers, props: true }, // :anyName - it matches everything that starts with /teams, also order matters as if we write /teams/new after this dynamic path it wont be renderd, adding props:true parameter means that "teamID" goes as props to the TeamMembers component to make it more reuseble
     { path: '/:notFound(.*)', component: NotFound }, //'/:notFound(.*)' means that any possible character combination will direct to component we chose or we can redirect too, and this must be last in routes list
   ],
   linkActiveClass: 'active', // we change link-active-class with active, that means that when link is activated with active class we can add some custom style
+  scrollBehavior(to, from, savedPosition) {
+    // describes scroll bar and have 3 arguments it has to return left: and top: parameters
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  },
+});
+
+//with navigation guards we can chek if user is autenticated and
+//give it acces to pages with next()
+//or not next(false)
+//beforeEachh function runs on every navigation
+router.beforeEach(function (to, from, next) {
+  next();
+});
+
+//it will run after navigation is confirmd
+router.afterEach(function (to, from) {
+  console.log(to, from);
 });
 
 const app = createApp(App);
